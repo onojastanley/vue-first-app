a327e4caa7d4e7ee29c65f7bb12eb30d18264e5e
Onoja Stanley <40136094+onojastanley@users.noreply.github.com>
2023-07-07 14:52:48 +0100

    level 5
    level 5

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks', // updated data value //
-            image: './assets/images/socks_green.jpg',
-            url: 'https://www.vuemastery.com/',
-            inventory: 100,
-            onSale: true
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            // solution
+            sizes: ['S', 'M', 'L', 'XL'],
+            // solution
+            variants: [
+              { id: 2234, color: 'green' },
+              { id: 2235, color: 'blue' },
+            ]
         }
     }
 })

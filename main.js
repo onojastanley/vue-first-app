8a06c87b4945898c51251fd19b22fa2298249af0
Onoja Stanley <40136094+onojastanley@users.noreply.github.com>
2023-07-07 09:21:26 +0100

    level 3
    level 3

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+<<<<<<< Updated upstream
 const product = 'Socks'
+=======
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks', // updated data value //
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
+        }
+    }
+})
+>>>>>>> Stashed changes
